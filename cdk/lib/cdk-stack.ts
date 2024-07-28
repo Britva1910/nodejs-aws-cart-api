695af6266236179c359c2f7b82242de2f8466a3d
Ivan Lashuk <bbritva1910@gmail.com>
2024-07-28 15:45:06 +0400

    feat: connect handler to DB

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -3,33 +3,49 @@ import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as path from 'path';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import { config } from 'dotenv';
+import { RestApi } from 'aws-cdk-lib/aws-apigateway';
+
+config({ path: path.join(__dirname, '..', '.env') });
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const cardServiceHandler = new lambda.Function(this, 'CardServiceHandler', {
+    const RDS_HOST = process.env.DB_HOST;
+    const RDS_PORT = process.env.DB_PORT;
+    const RDS_DB_NAME = process.env.DB_NAME;
+    const RDS_USERNAME = process.env.DB_USER;
+    const RDS_PASSWORD = process.env.DB_PASSWORD;
+
+    const cartServiceHandler = new lambda.Function(this, 'CardServiceHandler', {
       runtime: lambda.Runtime.NODEJS_20_X,
       code: lambda.Code.fromAsset(path.join(__dirname, '..', '..', 'dist')),
       handler: 'main.handler',
-      environment: {},
+      environment: {
+        DB_HOST: RDS_HOST!,
+        DB_PORT: RDS_PORT!,
+        DB_NAME: RDS_DB_NAME!,
+        DB_USER: RDS_USERNAME!,
+        DB_PASSWORD: RDS_PASSWORD!,
+      },
+      timeout: cdk.Duration.seconds(6),
     });
 
-    const api = new apigateway.RestApi(this, 'CardServiceAPI', {
-      restApiName: 'Card Service',
-      cloudWatchRole: true,
+    const api = new RestApi(this, 'CartAPI', {
+      restApiName: 'CartAPI',
       defaultCorsPreflightOptions: {
         allowOrigins: apigateway.Cors.ALL_ORIGINS,
         allowMethods: apigateway.Cors.ALL_METHODS,
-        allowHeaders: apigateway.Cors.DEFAULT_HEADERS,
       },
     });
 
-    const cardResource = api.root.addResource('card');
-    const cardLambdaIntegration = new apigateway.LambdaIntegration(
-      cardServiceHandler,
-    );
-    cardResource.addMethod('GET', cardLambdaIntegration);
-    cardResource.addMethod('POST', cardLambdaIntegration);
+    api.root.addProxy({
+      defaultIntegration: new apigateway.LambdaIntegration(cartServiceHandler),
+    });
+
+    new cdk.CfnOutput(this, 'CartAPIUrl', {
+      value: api.url,
+    });
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,34 @@
 import { Module } from '@nestjs/common';
-
 import { AppController } from './app.controller';
-
 import { CartModule } from './cart/cart.module';
 import { AuthModule } from './auth/auth.module';
 import { OrderModule } from './order/order.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from 'dotenv';
+
+config();
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+      logging: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }),
     AuthModule,
     CartModule,
     OrderModule,
   ],
-  controllers: [
-    AppController,
-  ],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
