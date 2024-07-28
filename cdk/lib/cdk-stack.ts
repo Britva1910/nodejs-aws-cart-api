31ac8efcd8ceb545f0e64bc718bc9a26b06eb57d
Ivan Lashuk <bbritva1910@gmail.com>
2024-07-28 13:03:21 +0400

    feat: add webpack config and serverless

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,35 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as path from 'path';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const cardServiceHandler = new lambda.Function(this, 'CardServiceHandler', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      code: lambda.Code.fromAsset(path.join(__dirname, '..', '..', 'dist')),
+      handler: 'main.handler',
+      environment: {},
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const api = new apigateway.RestApi(this, 'CardServiceAPI', {
+      restApiName: 'Card Service',
+      cloudWatchRole: true,
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: apigateway.Cors.ALL_METHODS,
+        allowHeaders: apigateway.Cors.DEFAULT_HEADERS,
+      },
+    });
+
+    const cardResource = api.root.addResource('card');
+    const cardLambdaIntegration = new apigateway.LambdaIntegration(
+      cardServiceHandler,
+    );
+    cardResource.addMethod('GET', cardLambdaIntegration);
+    cardResource.addMethod('POST', cardLambdaIntegration);
   }
 }
